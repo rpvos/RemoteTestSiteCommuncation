1fdd5fe8932fa864c40b07a6fc0e0211cee26da7
Rik <rik.vos01@gmail.com>
2024-03-06 11:33:53 +0100

    Fix sensor node message handler

--- include/message_handlers/RemoteTestSiteSensorNode.hpp
+++ include/message_handlers/RemoteTestSiteSensorNode.hpp
@@ -1,85 +1,18 @@
-#include "message_handlers/RemoteTestSiteSensorNode.hpp"
+#ifndef REMOTE_TEST_SITE_SENSOR_NODE_HPP_
+#define REMOTE_TEST_SITE_SENSOR_NODE_HPP_
 
-#include "message_handlers/RemoteTestSiteSensorNode.hpp"
+#include "interfaces/i_message_handler.hpp"
 
-#include <Arduino.h>
-void RemoteTestSiteSensorNode::HandleMessage(RemoteTestSite_Message message)
+class RemoteTestSiteSensorNode : public IMessageHandler
 {
-    /* Check if message is meant for this node */
-    if (message.has_target_id)
-    {
-        // TODO: check for id in handle or in connection handler
-        // if (this->id != message.target_id)
-        // {
-        //     /* Message not meant for this node. */
-        //     return true;
-        // }
-    }
+public:
+    void HandleMessage(RemoteTestSite_Message message) override;
 
-    switch (message.which_function_info)
-    {
-    case RemoteTestSite_Message_measurement_tag:
-        if (message.function_info.measurement.has_type && message.function_info.measurement.has_value)
-        {
-            Measurement(message.function_info.measurement.type, message.function_info.measurement.value);
-        }
+private:
+    bool Measurement(RemoteTestSite_MeasurementType type, float value);
+    bool UpdateFrequency(RemoteTestSite_MeasurementType type, RemoteTestSite_Timestamp frequency);
+    bool Response(RemoteTestSite_ResponseCode response_code);
+    bool UnknownMessageType(pb_size_t which_function_info);
+};
 
-        break;
-    case RemoteTestSite_Message_update_tag:
-        // TODO: make sitch for update types
-        UpdateFrequency(message.function_info.update.UpdateType.update_frequency.type, message.function_info.update.UpdateType.update_frequency.frequency);
-        break;
-    case RemoteTestSite_Message_response_tag:
-        if (message.function_info.response.has_response_code)
-        {
-            Response(message.function_info.response.response_code);
-        }
-
-        break;
-
-    default:
-        UnknownMessageType(message.which_function_info);
-        break;
-    }
-}
-
-bool RemoteTestSiteSensorNode::Measurement(RemoteTestSite_MeasurementType type, float value)
-{
-    Serial.print("Measurement of type: ");
-    Serial.println((int)type);
-    Serial.print("Value: ");
-    Serial.println((float)value);
-    return true;
-}
-
-bool RemoteTestSiteSensorNode::UpdateFrequency(RemoteTestSite_MeasurementType type, RemoteTestSite_Timestamp frequency)
-{
-    Serial.print("Update of type: ");
-    Serial.println((int)type);
-    Serial.print("Frequency in second:nanoseconds : ");
-    Serial.print((int)frequency.seconds);
-    Serial.print(':');
-    Serial.println((int)frequency.nanos);
-    return true;
-}
-
-bool RemoteTestSiteSensorNode::Response(RemoteTestSite_ResponseCode response_code)
-{
-    if (response_code == RemoteTestSite_ResponseCode::RemoteTestSite_ResponseCode_RESPONSE_CODE_OK)
-    {
-        Serial.println("Response OK");
-    }
-    else
-    {
-        Serial.print("Error code: ");
-        Serial.println((int)response_code);
-    }
-    return true;
-}
-
-bool RemoteTestSiteSensorNode::UnknownMessageType(pb_size_t which_function_info)
-{
-    Serial.print("Function unknown: ");
-    Serial.println((int)which_function_info);
-    return true;
-}
+#endif // #ifndef REMOTE_TEST_SITE_SENSOR_NODE_HPP_
