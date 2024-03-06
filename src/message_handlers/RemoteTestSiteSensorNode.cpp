8f3ba827c4cae59c0af0599e60d5922c500cbe5a
Rik <rik.vos01@gmail.com>
2024-03-06 10:59:39 +0100

    Add sensor node message handler

--- src/message_handlers/RemoteTestSiteSensorNode.cpp
+++ src/message_handlers/RemoteTestSiteSensorNode.cpp
@@ -1,5 +1,18 @@
-// #include "message_handlers/RemoteTestSiteSensorNode.hpp"
+#ifndef REMOTE_TEST_SITE_SENSOR_NODE_HPP_
+#define REMOTE_TEST_SITE_SENSOR_NODE_HPP_
 
-// RemoteTestSiteSensorNode::RemoteTestSiteSensorNode() : communication_adapter(this)
-// {
-// }
+#include "interfaces/i_message_handler.hpp"
+
+class RemoteTestSiteSensorNode : public IMessageHandler
+{
+public:
+    void HandleMessage(RemoteTestSite_Message message) override;
+
+private:
+    bool Measurement(RemoteTestSite_MeasurementType type, float value);
+    bool UpdateFrequency(RemoteTestSite_MeasurementType type, RemoteTestSite_Timestamp frequency);
+    bool Response(RemoteTestSite_ResponseCode response_code);
+    bool UnknownMessageType(pb_size_t which_function_info);
+};
+
+#endif // #ifndef REMOTE_TEST_SITE_SENSOR_NODE_HPP_
